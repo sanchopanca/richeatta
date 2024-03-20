57b54ad9027f37c77f0f940b6d2a4bf555819beb
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2024-03-20 14:46:51 +0100

    Move opening of process handle to a common place.
    It's the same code in every method. If we create more methods,
    chances are we are gonna need it there too

--- src/memory/windows.rs
+++ src/memory/windows.rs
@@ -13,12 +13,45 @@ use super::first_search;
 use super::OSMemory;
 
 pub struct Windows {
+    #[allow(dead_code)]
     pid: u32,
+    process_handle: *mut winapi::ctypes::c_void,
 }
 
 impl Windows {
     pub fn new(pid: i32) -> Self {
-        Windows { pid: pid as u32 }
+        let pid = pid as u32;
+        let process_handle = unsafe {
+            OpenProcess(
+                PROCESS_QUERY_INFORMATION
+                    | PROCESS_VM_READ
+                    | PROCESS_VM_WRITE
+                    | PROCESS_VM_OPERATION,
+                0,
+                pid,
+            )
+        };
+
+        if process_handle.is_null() {
+            panic!(
+                "Failed to open process with pid: {}. Error: {}",
+                pid,
+                unsafe { GetLastError() }
+            );
+        }
+
+        Windows {
+            pid,
+            process_handle,
+        }
+    }
+}
+
+impl Drop for Windows {
+    fn drop(&mut self) {
+        unsafe {
+            CloseHandle(self.process_handle);
+        }
     }
 }
 
@@ -26,31 +59,17 @@ impl<T: Integer> OSMemory<T> for Windows {
     fn modify_at_address(&self, address: usize, value: T) {
         let value = value.to_ne_bytes();
 
-        // Open a handle to the process with write and operation permissions
-        let process_handle =
-            unsafe { OpenProcess(PROCESS_VM_WRITE | PROCESS_VM_OPERATION, 0, self.pid) };
-
-        if process_handle.is_null() {
-            eprintln!("Failed to open process");
-            return;
-        }
-
         // Attempt to write data to the specified memory address
         let mut bytes_written = 0;
         let write_result = unsafe {
             WriteProcessMemory(
-                process_handle,
+                self.process_handle,
                 address as *mut _,
                 value.as_ptr() as *const _,
                 value.len(),
                 &mut bytes_written,
             )
         };
-
-        // Close the handle after the operation
-        unsafe {
-            CloseHandle(process_handle);
-        }
 
         if write_result == 0 {
             eprintln!("Failed to write memory");
@@ -60,23 +79,13 @@ impl<T: Integer> OSMemory<T> for Windows {
     }
 
     fn search_everywhere(&self, value: T) -> Vec<usize> {
-        let process_handle =
-            unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0, self.pid) };
-
-        if process_handle.is_null() {
-            println!("Failed to open process. Error: {}", unsafe {
-                GetLastError()
-            });
-            return Vec::new();
-        }
-
         let mut candidates = Vec::new();
         let mut base_address = 0;
         let mut memory_info: MEMORY_BASIC_INFORMATION = unsafe { zeroed() };
 
         while unsafe {
             VirtualQueryEx(
-                process_handle,
+                self.process_handle,
                 base_address as *const _,
                 &mut memory_info,
                 size_of::<MEMORY_BASIC_INFORMATION>(),
@@ -90,7 +99,7 @@ impl<T: Integer> OSMemory<T> for Windows {
 
                 let read_success = unsafe {
                     ReadProcessMemory(
-                        process_handle,
+                        self.process_handle,
                         memory_info.BaseAddress,
                         buffer.as_mut_ptr() as *mut _,
                         memory_info.RegionSize,
@@ -106,23 +115,10 @@ impl<T: Integer> OSMemory<T> for Windows {
             // Move to the next memory region
             base_address = (memory_info.BaseAddress as usize) + memory_info.RegionSize;
         }
-        unsafe {
-            CloseHandle(process_handle);
-        }
         candidates
     }
 
     fn search_among_candidates(&self, value: T, candidates: &[usize]) -> Vec<usize> {
-        let process_handle =
-            unsafe { OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, 0, self.pid) };
-
-        if process_handle.is_null() {
-            println!("Failed to open process. Error: {}", unsafe {
-                GetLastError()
-            });
-            return Vec::new();
-        }
-
         let mut remaining_candidates = Vec::new();
 
         for &address in candidates {
@@ -130,7 +126,7 @@ impl<T: Integer> OSMemory<T> for Windows {
             let mut value_at_address = T::new();
             let read_success = unsafe {
                 ReadProcessMemory(
-                    process_handle,
+                    self.process_handle,
                     address as *const _,
                     &mut value_at_address as *mut _ as *mut _,
                     size_of::<T>(),
